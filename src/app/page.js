f0b7e114e13be913f3489385dd47ac1d56dfc6e7
Chang <>
2024-04-01 19:24:58 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import dayjs from "dayjs";
-import Webcam from "react-webcam";
+// import Webcam from "react-webcam";
+// import { ReactMediaRecorder } from "react-media-recorder";
 
 export default function Home() {
   const now = dayjs();
@@ -15,61 +16,13 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-        <a
-          href="https://day.js.org/docs/en/installation/installation"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            day.js <span>-&gt;</span>
-          </h2>
-          <p></p>
-        </a>
-
-        <a
-          href="https://moment.github.io/luxon/#/?id=luxon"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            luxon <span>-&gt;</span>
-          </h2>
-          <p></p>
-        </a>
-
-        <a
-          href="https://date-fns.org/v3.6.0/docs/formatDistanceStrict"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            date-fns <span>-&gt;</span>
-          </h2>
-          <p></p>
-        </a>
-
-        <a
-          href="https://momentjs.com/docs/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            momment.js <span>-&gt;</span>
-          </h2>
-          <p></p>
-        </a>
-      </div>
-      <div className={styles.description}>
+      <div className={styles.grid}>{/* <Webcam /> */}</div>
+      {/* <div className={styles.description}>
         <p>
           {now} || {date1.toJSON()} || {date2.toJSON()} || {date3.toJSON()}{" "}
         </p>
       </div>
-      <Webcam />;
+
       <div className={styles.center}>
         <Image
           className={styles.logo}
@@ -79,7 +32,7 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+      </div> */}
     </main>
   );
 }
