523cc34e24e738680793be6ea852e3a0be9c2f0a
Chang <>
2024-05-09 15:16:59 +0900

    dd

--- next.config.js
+++ next.config.js
@@ -5,3 +5,12 @@
 // module.exports = withPWA({
 //   // next.js config
 // });
+
+// import pluginOptions from "next-pwa";
+
+// const withPWA = pluginOptions({ dest: "public" });
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default withPWA(nextConfig);
